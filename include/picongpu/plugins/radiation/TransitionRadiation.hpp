5c52f3123ab3352dc0e146024e593788859e8246
Finn-Ole Carstens <finn-ole.carstens@mailbox.tu-dresden.de>
2019-03-04 08:46:43 +0100

    create file

---
+++ include/picongpu/plugins/radiation/TransitionRadiation.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "picongpu/simulation_defines.hpp"
+
+#include "picongpu/plugins/ISimulationPlugin.hpp"
+
+namespace picongpu
+{
+using namespace pmacc;
+
+} // namespace picongpu
